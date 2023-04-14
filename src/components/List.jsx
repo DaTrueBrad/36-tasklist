6bbbd59a090700a0a7e20d659b37b09ca87c73df
Brady Bott <bradybott@Bradys-MacBook-Pro.local>
2023-04-14 10:04:15 -0600

    finished

---
+++ src/components/List.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Task from './Task'
+
+const List = (props) => {
+
+  let taskDisplay = props.taskList.map((task, index) => {
+    return <Task task={task} />
+  })
+
+  return (
+    <div className='col-container'>
+      <h2>My Tasks</h2>
+      {taskDisplay}
+    </div>
+  )
+}
+
+export default List
